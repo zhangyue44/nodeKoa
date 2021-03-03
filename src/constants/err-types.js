67b5c5973ca6193cb112d2c7cabc1fbe359eeee9
zhangyue44 <zy1617768774@163.com>
2021-03-03 13:22:03 +0800

    sixth commit

--- src/constants/err-types.js
+++ src/constants/err-types.js
@@ -3,7 +3,7 @@ const USER_ALREADY_EXISTS = 'user_already_exists';
 const USER_DOES_NOT_EXISTS = 'user_does_not_exists';
 const PASSWORD_IS_INCORRENT = 'password_is_incorrent';
 const UNAUTHORIZATION = 'UNAUTHORIZATION';
-// const UNPERMISSION = 'unpermission';
+const UNPERMISSION = 'unpermission';
 
 module.exports = {
   NAME_OR_PASSWORD_IS_REQUIRED,
@@ -11,5 +11,5 @@ module.exports = {
   USER_DOES_NOT_EXISTS,
   PASSWORD_IS_INCORRENT,
   UNAUTHORIZATION,
-  // UNPERMISSION
+  UNPERMISSION
 }
